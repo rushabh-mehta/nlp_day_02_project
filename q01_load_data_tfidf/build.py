beec2140b1d1fd49aed513d9c653378b120d0ca0
rushabh-mehta <rushabh123@gmail.com>
2019-01-04 14:28:41 +0000

    Done

--- q01_load_data_tfidf/build.py
+++ q01_load_data_tfidf/build.py
@@ -1,10 +1,18 @@
+# %load q01_load_data_tfidf/build.py
 # Default imports
 
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.datasets import load_files
+from nltk.tokenize import TreebankWordTokenizer
 
+path = 'data/sessions.csv'
 # Write your solution here :
-
-
+def q01_load_data_tfidf(path,max_df=0.95,min_df=2,no_features=1000):
+    dataset = pd.read_csv(path)
+    tfidf_vectorizer = TfidfVectorizer(max_df=max_df, min_df=min_df, max_features=no_features, stop_words='english')
+    tfidf = tfidf_vectorizer.fit_transform(dataset['talkTitle'])
+    tfidf_feature_names = tfidf_vectorizer.get_feature_names()
+    return dataset, tfidf, tfidf_feature_names
 
 
